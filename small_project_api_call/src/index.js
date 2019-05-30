7241e009f24ec6b384b4d891a2d6a1a2f9f6ca0d
Michael-Mignon <mmignon.michaelp@gmail.com>
2019-05-30 11:57:09 -0400

    use: 'npm start' command in base directory to run project on localhost

---
+++ small_project_api_call/src/index.js
@@ -0,0 +1,8 @@
+import React from "../node_modules/react";
+import ReactDOM from '../node_modules/react-dom';
+import './index.css';
+import App from './App';
+
+// const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(<App />, document.getElementById('root'));
+
